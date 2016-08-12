69acacb29f8a4a4d1da21dc1278fdfbb9b271d6e
Eric Tran <erictran@gmail.com>
2016-08-12 16:26:32 -0700

    index updates with ev.php

--- hw2_files/ev.php
+++ hw2_files/ev.php
@@ -4,12 +4,12 @@ header('Content-Type: text/html');
 echo "<html><head><title>Environment Variables</title>";
 
 echo "</head><body align='center'>";
-echo "<p>";
 
 foreach ($_ENV as $name => $ky) {
-	echo "$name : ".$ky. "<br>";
+	echo "<p>$name : ".$ky. "</p><br>";
 }
 
-echo "</p>";
+
+
 echo"</body></html>";
 ?>
