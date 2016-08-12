1950f2786ef3f2bfdc2e18f9d40e5aa140049611
Eric Tran <erictran@gmail.com>
2016-08-12 16:15:01 -0700

    ev.php

---
+++ hw2_files/ev.php
@@ -0,0 +1,15 @@
+
+<?php
+header('Content-Type: text/html'); 
+echo "<html><head><title>Environment Variables</title>";
+
+echo "</head><body align='center'>";
+echo "<p>"
+
+foreach ($_ENV as $name => $ky) {
+	echo "$name : ".$ky. "<br>";
+}
+
+echo "</p>"
+echo"</body></html>";
+?>
