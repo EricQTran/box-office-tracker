f87023335eaab6afd147a9c2a9861d52d6df392e
Eric Tran <erictran@gmail.com>
2016-08-12 16:23:21 -0700

    ev.php fixes typo

--- hw2_files/ev.php
+++ hw2_files/ev.php
@@ -4,12 +4,12 @@ header('Content-Type: text/html');
 echo "<html><head><title>Environment Variables</title>";
 
 echo "</head><body align='center'>";
-echo "<p>"
+echo "<p>";
 
 foreach ($_ENV as $name => $ky) {
 	echo "$name : ".$ky. "<br>";
 }
 
-echo "</p>"
+echo "</p>";
 echo"</body></html>";
 ?>
