9f10fcc9de994dbe3238a3b46fe08c856986fead
Eric Tran <erictran@gmail.com>
2016-08-13 18:38:41 -0700

    adds form.php and echo

---
+++ hw2_files/echo.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: text/html'); 
+echo "<html><head><title>Form Echo</title>";
+
+echo "</head><body><h1>";
+
+echo "Welcome $_POST['firstname']";
+
+echo "</h1>";
+
+echo"</body></html>";
+?>
+
