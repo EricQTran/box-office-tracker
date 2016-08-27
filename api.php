86cedf7c769da75e869d924d5b1f94f7ea97194d
cse135-et <erictran@gmail.com>
2016-08-27 22:48:02 +0000

    api

--- api.php
+++ api.php
@@ -1,46 +1,9 @@
 <?php
 
 require_once 'config.inc';
-echo "hello"; 
-// what method is in play
-$method = $_SERVER['REQUEST_METHOD'];
+  
 
-// grab the URI
-$request = $_SERVER['REQUEST_URI'];
-
-// read the message if any
-$input = json_decode(file_get_contents('php://input'),true);
-
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-
-$key = array_shift($request)+0;
-
-
-$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
-$values = array_map(function ($value) use ($conn) {
-  if ($value===null) return null;
-  return mysqli_real_escape_string($conn,(string)$value);
-},array_values($input));
- 
-// build the SET part of the SQL command
-$set = '';
-for ($i=0;$i<count($columns);$i++) {
-  $set.=($i>0?',':'').'`'.$columns[$i].'`=';
-  $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
-}
- 
- 
-
-switch ($method) {
-  case 'GET':
-    $sql = "select * from users".($key?" where user_id=$key":''); break;
-  case 'PUT':
-    $sql = "update users set $set where user_id=$key"; break;
-  case 'POST':
-    $sql = "insert into users set $set"; break;
-  case 'DELETE':
-    $sql = "delete from users where user_id=$key"; break;
-}
+$sql = "select * from movies";
 
 
 $result = mysqli_query($conn, $sql);
@@ -51,7 +14,9 @@ if (!$result) {
  }
 
 // print results, insert id or affected row count
-if ($method == 'GET') {
+echo json_encode(mysqli_fetch_object($result));
+
+/*if ($method == 'GET') {
   if (!$key) echo '[';
   for ($i=0;$i<mysqli_num_rows($result);$i++) {
     echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
@@ -62,7 +27,9 @@ if ($method == 'GET') {
 } else {
   echo mysqli_affected_rows($conn);
 }
- 
+ */
+
+
 // close mysql connection
 mysqli_close($conn);
 
