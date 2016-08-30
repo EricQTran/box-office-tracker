b7919f89cc563b89335d84344c7e8e18779f8ad6
Eric Tran <erictran@gmail.com>
2016-08-29 18:35:30 -0700

    13

--- api.php
+++ api.php
@@ -12,7 +12,7 @@ $request = $_SERVER['REQUEST_URI'];
 // read the message if any
 $input = json_decode(file_get_contents('php://input'),true);
 
-print_r($input->title);
+echo $input[0]->title;
 
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
